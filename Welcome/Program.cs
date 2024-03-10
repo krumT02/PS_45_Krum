2cb94d2b728f51a47e7037f797fd633fa819985a
Krum <Krum@DESKTOP-P2SQRUV>
2024-03-10 16:28:12 +0200

    Experementing

--- Welcome/Program.cs
+++ Welcome/Program.cs
@@ -13,7 +13,7 @@ namespace Welcome
             
             UserViewModel viewModel = new UserViewModel(user);
             Console.WriteLine(user.Password); Console.WriteLine(viewModel.Password);
-            viewModel.Password = "121232A";
+            viewModel.Password = "121232AAAA";
             Console.WriteLine(viewModel.Password);
             UserView view = new UserView(viewModel);
             view.Display();
